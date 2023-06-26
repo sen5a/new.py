732deb092e0ce5112fc43d0799f968fb5243bc37
sen5a <aleks.kurochkin.97@mail.ru>
2023-06-26 14:32:55 +0300

    'd'

--- new.py
+++ new.py
@@ -1,8 +1,11 @@
 print('Я справлюсь')
 print('Получается')
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 print('mail.com and mail.py')
 =======
+=======
+>>>>>>> Stashed changes
 я иду ====
 >>>>>>> Stashed changes
 я лечу дальше
