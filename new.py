594f2109dd90e85f7c2fb69650c1235126ba9ddd
sen5a <aleks.kurochkin.97@mail.ru>
2023-06-26 14:39:17 +0300

    'new'

--- new.py
+++ new.py
@@ -1,4 +1,9 @@
 print('Я справлюсь')
 print('Получается')
+<<<<<<< Updated upstream
 print('mail.com and mail.py')
 def distance:
+=======
+я иду ====
+я лечу дальше ++++++++++
+>>>>>>> Stashed changes
